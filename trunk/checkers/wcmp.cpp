cf88610d4eacc6aa7e6fbcfb9f21b8972ff0c943
mirzayanovmr@gmail.com <mirzayanovmr@gmail.com@09e2fada-8250-11dd-aace-ddc5e87cd236>
2012-12-14 15:00:37 +0000

    - use readWordTo in wcmp to speedup it.

--- trunk/checkers/wcmp.cpp
+++ trunk/checkers/wcmp.cpp
@@ -7,25 +7,26 @@ int main(int argc, char * argv[])
     setName("compare sequences of tokens");
     registerTestlibCmd(argc, argv);
 
-    std::string strAnswer;
-
     int n = 0;
+
+    string j, p;
 
     while (!ans.seekEof() && !ouf.seekEof()) 
     {
         n++;
-        std::string j = ans.readWord();
-        std::string p = ouf.readWord();
-        strAnswer = p;
-        quitif(j != p,
-            _wa, "%d%s words differ - expected: '%s', found: '%s'",
-            n, englishEnding(n).c_str(), compress(j).c_str(), compress(p).c_str());
+
+        ans.readWordTo(j);
+        ouf.readWordTo(p);
+        
+        if (j != p)
+            quitf(_wa, "%d%s words differ - expected: '%s', found: '%s'",
+                n, englishEnding(n).c_str(), compress(j).c_str(), compress(p).c_str());
     }
 
     if (ans.seekEof() && ouf.seekEof())
     {
         if (n == 1)
-            quitf(_ok, "%s", compress(strAnswer).c_str());
+            quitf(_ok, "\"%s\"", compress(j).c_str());
         else
             quitf(_ok, "%d words", n);
     }
