11200c8e13be1df11257a3d2686a3611a2a36400
mirzayanovmr@gmail.com <mirzayanovmr@gmail.com@09e2fada-8250-11dd-aace-ddc5e87cd236>
2015-01-20 17:38:25 +0000

    - C++11 support.

--- trunk/checkers/rcmp.cpp
+++ trunk/checkers/rcmp.cpp
@@ -6,14 +6,14 @@ const double EPS = 1.5E-6;
 
 int main(int argc, char * argv[])
 {
-    setName("compare two doubles, maximal absolute error = %.10lf", EPS);
+    setName("compare two doubles, maximal absolute error = %.10f", EPS);
     registerTestlibCmd(argc, argv);
     
     double ja = ans.readDouble();
     double pa = ouf.readDouble();
 
     if (fabs(ja - pa) > EPS + 1E-15)
-        quitf(_wa, "expected %.10lf, found %.10lf", ja, pa);
+        quitf(_wa, "expected %.10f, found %.10f", ja, pa);
     
-    quitf(_ok, "answer is %.10lf", ja);
+    quitf(_ok, "answer is %.10f", ja);
 }
--- trunk/checkers/rncmp.cpp
+++ trunk/checkers/rncmp.cpp
@@ -7,7 +7,7 @@ const double EPS = 1.5E-5;
 
 int main(int argc, char * argv[])
 {
-    setName("compare two sequences of doubles, maximal absolute error = %.10lf", EPS);
+    setName("compare two sequences of doubles, maximal absolute error = %.10f", EPS);
     registerTestlibCmd(argc, argv);
 
     int n = 0;
@@ -17,7 +17,7 @@ int main(int argc, char * argv[])
         double j = ans.readDouble();
         double p = ouf.readDouble();
         if (fabs(j - p) > EPS + 1E-15)
-            quitf(_wa, "%d%s numbers differ - expected: '%.10lf', found: '%.10lf'", n, englishEnding(n).c_str(), j, p);
+            quitf(_wa, "%d%s numbers differ - expected: '%.10f', found: '%.10f'", n, englishEnding(n).c_str(), j, p);
     }
 
     quitf(_ok, "%d numbers", n);
