398d05f4daecd854290f0e77af246d57872835d0
Tran Quang Loc <quangloctran99@gmail.com>
2022-07-26 21:57:22 +0700

    Add tests for default-value and unused opts.

--- tests/test-004_use-test.h/tests/test-opts.cpp
+++ tests/test-004_use-test.h/tests/test-opts.cpp
@@ -42,4 +42,84 @@ TEST(opts) {
         ensure_exit(3, [](){ opt<int>(14);});
         ensure_exit(3, [](){ opt<unsigned long long>("a");});
     }
+    
+    {
+        // Testing opt with default value and unused opt.
+        // The following function are opts of the generators/gen-array-with-opt.cpp
+        // Test is conducted by throwing arguments to this function.
+        struct GenArrayOpts {
+            int test_count;
+            int sum_n, min_n;
+            int min_value, max_value, value_bias;
+        };
+        auto gen_array_with_opt_parse_opt = [](const string& opts) {
+            {
+                auto parts = split(opts, ' ');
+                vector<const char*> opts(parts.size() + 1);
+                opts[0] = "./gen-array-with-opt";
+                for (size_t i = 0; i < parts.size(); ++i) {
+                    opts[i + 1] = parts[i].c_str();
+                }
+                prepareOpts(opts.size(), (char**)opts.data());
+            }
+            {
+                GenArrayOpts res;
+                res.test_count = opt<int>("test-count");
+                res.sum_n = opt<int>("sum-n");
+                res.min_n = opt<int>("min-n", 1);
+                
+                res.min_value = opt<int>("min-value", 1);
+                res.max_value = opt<int>("max-value", 1000 * 1000 * 1000);
+                res.value_bias = opt<int>("value-bias", 0);
+                
+                check_unused_opt();
+                return res;
+            }
+        };
+        
+        // missing test-count
+        ensure_exit(3, [&] { gen_array_with_opt_parse_opt(""); });
+        // missing sum-n
+        ensure_exit(3, [&]{ gen_array_with_opt_parse_opt("-test-count 3"); });
+        // min-value, not min-val
+        ensure_exit(3, [&]{ gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10 -min-val 1"); } );
+        // max-value, not max-val
+        ensure_exit(3, [&]{ gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10 -min-value 1 -max-val 20"); } );
+        // min-n, not min-length
+        ensure_exit(3, [&]{ gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10 -min-length 3 -min-value 10 -max-value 20"); });
+        // value-bias, not bias-value
+        ensure_exit(3, [&]{ gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10 -min-n 3 -min-value 10 -max-value 20 -bias-value 3"); });
+        
+        // OK cases
+        {
+            auto t = gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10");
+            ensure(t.test_count == 3);
+            ensure(t.sum_n == 10);
+            ensure(t.min_n == 1);
+            ensure(t.min_value == 1);
+            ensure(t.max_value == 1000 * 1000 * 1000);
+            ensure(t.value_bias == 0);
+        }
+        
+        {
+            auto t = gen_array_with_opt_parse_opt("-test-count 3 -sum-n 10 -min-value 10");
+            ensure(t.test_count == 3);
+            ensure(t.sum_n == 10);
+            ensure(t.min_n == 1);
+            ensure(t.min_value == 10);
+            ensure(t.max_value == 1000 * 1000 * 1000);
+            ensure(t.value_bias == 0);
+        }
+        
+        {
+            auto t = gen_array_with_opt_parse_opt("-test-count 5 -sum-n 100 -min-n 3 -min-value 10 -max-value 20 -value-bias 3 ");
+            ensure(t.test_count == 5);
+            ensure(t.sum_n == 100);
+            ensure(t.min_n == 3);
+            ensure(t.min_value == 10);
+            ensure(t.max_value == 20);
+            ensure(t.value_bias == 3);
+        }
+    }
 }
+
