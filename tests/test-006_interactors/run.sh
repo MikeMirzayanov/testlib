232769eb25c00ac07fcb68cdbaaf4186abfbc7f6
mikemirzayanov <mirzayanovmr@gmail.com>
2022-08-12 23:12:44 +0300

    dos2unix -> tr

--- tests/test-006_interactors/run.sh
+++ tests/test-006_interactors/run.sh
@@ -8,6 +8,6 @@ fi
 
 bash ../scripts/compile src/interactor-a-plus-b.cpp
 bash ../scripts/test-ref r-interactor-a-plus-b-1-1 "$VALGRIND" ./interactor-a-plus-b files/"$os"/input.01 output.01 < files/"$os"/participant.01
-dos2unix output.01
-bash ../scripts/test-ref r-interactor-a-plus-b-1-2 cat output.01
-rm -f interactor-a-plus-b interactor-a-plus-b.exe output.01
+tr -d '\r' < output.01 > output.01.nix
+bash ../scripts/test-ref r-interactor-a-plus-b-1-2 cat output.01.nix
+rm -f interactor-a-plus-b interactor-a-plus-b.exe output.01 output.01.nix
