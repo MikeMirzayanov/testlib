ff6ca5d6594f70efd3ee1d541c3cd727b2bc2ce6
mikemirzayanov <mirzayanovmr@gmail.com>
2023-09-03 22:50:51 +0300

    wip

--- tests/run.sh
+++ tests/run.sh
@@ -169,10 +169,10 @@ if [[ "$machine" == "Windows" && ("$ARGS_CPP" == "" || "$ARGS_CPP" == "msvc") ]]
             if [[ -f $vcvars_bat_file ]]; then
               echo "Compiler Visual Studio $version ($vs_release-$bits) has been found"
               echo "echo d1" >dovcvars.bat
-              #echo call \""$vcvars_bat_file"\" >>dovcvars.bat
-              #echo "echo d2" >>dovcvars.bat
-              #echo "bash -c export > vcvars.env" >>dovcvars.bat
-              #echo "echo d3" >>dovcvars.bat
+              echo call \""$vcvars_bat_file"\" >>dovcvars.bat
+              echo "echo d2" >>dovcvars.bat
+              echo "bash -c export > vcvars.env" >>dovcvars.bat
+              echo "echo d3" >>dovcvars.bat
 
               echo "File dovcvars.bat:"
               cat dovcvars.bat
