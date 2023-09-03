79f54c2962bbe38c7522a1ab1418b65cdda58c4c
mikemirzayanov <mirzayanovmr@gmail.com>
2023-09-03 22:45:14 +0300

    wip

--- tests/runner.py
+++ tests/runner.py
@@ -1,2 +1,9 @@
 import subprocess
-subprocess.run(["testme.bat"])
+import sys
+
+if len(sys.argv) > 1:
+    file_path = sys.argv[1]
+    subprocess.run([file_path])
+else:
+    print("Use python runner.py <file>")
+    sys.exit(1)
