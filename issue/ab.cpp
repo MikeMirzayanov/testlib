7c5e9e25a861b392d5f1b39face6129ab05e7113
mikemirzayanov <mirzayanovmr@gmail.com>
2023-09-26 23:02:24 +0300

    investigate windows issue

---
+++ issue/ab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
---
+++ issue/run.bat
@@ -0,0 +1,19 @@
+echo gcc case
+clang++ -O2 -o ab-clang.exe ab.cpp
+ab-clang.exe < input.txt > output.clang
+echo -
+echo %ERRORLEVEL%
+echo -
+type output.clang
+echo -
+
+echo clang case
+g++ -O2 -o ab-gcc.exe ab.cpp
+ab-gcc.exe < input.txt > output.gcc
+echo -
+echo %ERRORLEVEL%
+echo -
+type output.gcc
+echo -
+
+echo end
