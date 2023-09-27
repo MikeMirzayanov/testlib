560196ba2c707dd7e3a5f119bf4ab3d780ec53c6
mikemirzayanov <mirzayanovmr@gmail.com>
2023-09-27 21:08:08 +0300

    static

--- issue/run.sh
+++ issue/run.sh
@@ -20,7 +20,7 @@ fi
 echo "clang case"
 clang++ --version
 rm -f ab-clang$EXE_EXT
-clang++ -O2 -o ab-clang$EXE_EXT ab.cpp
+clang++ -static -O2 -o ab-clang$EXE_EXT ab.cpp
 cp -f input.txt i
 ./invoke.sh ./ab-clang
 rm -f ab-clang$EXE_EXT
@@ -28,8 +28,8 @@ rm -f ab-clang$EXE_EXT
 echo "gcc case"
 g++ --version
 rm -f ab-gcc$EXE_EXT
-#eval g++ --std=c++11 -Wpedantic -Werror -O2 -o ab-gcc$EXE_EXT ab.cpp
-eval g++ -O0 -o ab-gcc$EXE_EXT ab.cpp
+#eval g++ --std=c++11 -Wpedantic -Werror -static -O2 -o ab-gcc$EXE_EXT ab.cpp
+eval g++ -static -O2 -o ab-gcc$EXE_EXT ab.cpp
 cp -f input.txt i
 ./invoke.sh ./ab-gcc
 rm -f ab-gcc$EXE_EXT
@@ -37,7 +37,7 @@ rm -f ab-gcc$EXE_EXT
 echo "case-nval clang case"
 clang++ --version
 rm -f case-nval-clang$EXE_EXT
-clang++ -O2 -o case-nval-clang$EXE_EXT case-nval.cpp
+clang++ -static -O2 -o case-nval-clang$EXE_EXT case-nval.cpp
 cp -f input.01 i
 ./invoke.sh ./case-nval-clang --testMarkupFileName a
 cat a
@@ -47,8 +47,8 @@ rm -f case-nval-clang$EXE_EXT
 echo "case-nval gcc case"
 g++ --version
 rm -f case-nval-gcc$EXE_EXT
-#eval g++ --std=c++11 -Wpedantic -Werror -O2 -o case-nval-gcc$EXE_EXT case-nval.cpp
-eval g++ --std=c++11 -O1 -o case-nval-gcc$EXE_EXT case-nval.cpp
+#eval g++ --std=c++11 -Wpedantic -Werror -static -O2 -o case-nval-gcc$EXE_EXT case-nval.cpp
+eval g++ --std=c++11 -static -O2 -o case-nval-gcc$EXE_EXT case-nval.cpp
 cp -f input.01 i
 ./invoke.sh ./case-nval-gcc --testMarkupFileName a
 cat a
