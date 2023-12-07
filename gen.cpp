4fe6ee81fa6152b740924072c892ffee76783e24
wylovecat <2438626764@qq.com>
2023-12-07 13:38:32 +0800

    修改缩进

--- gen.cpp
+++ gen.cpp
@@ -5,26 +5,31 @@ using namespace std;
 using namespace tree_generator_by_ouuan;
 typedef long long ll;
 const int N=2e5+5;
-int x[N];
-int s[N];
+int a[N];
 int main(int argc, char *argv[]){
-	registerGen(argc, argv, 1);
-    /*****************************/
-	int k=opt<int>(1);
-	int L=1,R=1000;
-	if(k>=1&&k<=5){
-		L=1;R=10;
-	}else{
-		L=10;R=100;
-	}
-    int n=rnd.next(L,R);
-    int x=rnd.next(2,10);
-    Tree t;
+        registerGen(argc, argv, 1);
+        /*****************************/
+        int k=opt<int>(1);
+        int L=1,R=1000;
+        if(k>=1&&k<=5){
+            L=1;R=10;
+        }else{
+            L=10;R=100;
+        }
+        int n=rnd.next(L,R);
+        int x=rnd.next(2,10);
+        Tree t;
 
-    t.complete(n,x,0);////生成n个节点的x-1叉随机树 ，1为根
-    cout<<n<<" "<<x-1<<endl;
-    cout<<t;
+        t.complete(n,x,0);////生成n个节点的x-1叉随机树 ，1为根
+        cout<<n<<" "<<x-1<<endl;
+        cout<<t;
 
-
+        for(int i=1;i<=n;i++){
+            a[i]=i;
+        }
+        shuffle(a+1,a+n+1);
+        for(int i=1;i<=n;i++){
+            cout<<a[i]<<" ";
+        }
 	return 0;
 }
