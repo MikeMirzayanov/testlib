073e45723641322004f05428c130031fd4d07fb6
wylovecat <2438626764@qq.com>
2023-12-07 15:47:27 +0800

    修改

--- std.cpp
+++ std.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N=10005;
-int L[N],H[N],R[N],n;
-int a[N<<1],b[N<<1];
+const int N=1e6+5;
+vector<int> G[N];
+int dp[N];
+void dfs(int u,int f){
+    dp[u]=1;
+    for(auto v:G[u]){
+        if(f==v) continue;
+        dfs(v,u);
+        dp[u]+=dp[v];
+    }
+}
 int main(){
-	int l,h,r;
-	while(cin>>l>>h>>r){
-		n++;
-		L[n]=l;H[n]=h;R[n]=r;
-		a[2*n-1]=l;
-		a[2*n]=r;
-	}
-	sort(a+1,a+2*n+1);
-	int m=unique(a+1,a+2*n+1)-a-1;
-	for(int i=1;i<=n;i++){
-		int kl=lower_bound(a+1,a+m+1,L[i])-a;
-		int kr=lower_bound(a+1,a+m+1,R[i])-a;
-		for(int j=kl;j<kr;j++){
-			b[j]=max(b[j],H[i]);
-		}
-	}
-	for(int i=1;i<=m;i++){
-		if(b[i]!=b[i-1]){
-			cout<<a[i]<<" "<<b[i]<<" ";
-		}
-	}
+    int n,m,u,v;
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<n;i++){
+        scanf("%d%d",&u,&v);
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    dfs(1,1);
+    for(int i=1;i<=m;i++){
+        scanf("%d",&u);
+        printf("%d\n",dp[u]);
+    }
 	return 0;
 }
