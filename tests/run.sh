3b0b493d2d184e2bb156e527fc53fb2dadb8b614
mikemirzayanov <mirzayanovmr@gmail.com>
2026-02-10 19:21:21 +0300

    fix tests-windows-2025-vs2026

--- tests/run.sh
+++ tests/run.sh
@@ -219,7 +219,7 @@ if [[ "$MACHINE" == "Windows" && "$ARGS_CPP" == "" ]]; then
 fi
 
 for compiler in "${COMPILERS[@]}"; do
-  for version in 0 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+  for version in 0 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
     if [[ "$ARGS_CPP_VERSIONS" == "," || "$ARGS_CPP_VERSIONS" == *,$version,* ]]; then
       cpp="$compiler-$version"
       if [[ "$version" == 0 ]]; then
