d73b5af115a65185eb851ad34551fc80356dcb5e
mikemirzayanov <mirzayanovmr@gmail.com>
2026-08-23 03:53:48 +0300

    Discover Visual Studio 2026 installations

--- tests/run.sh
+++ tests/run.sh
@@ -168,7 +168,7 @@ if [[ "$machine" == "Windows" && ("$ARGS_CPP" == "" || "$ARGS_CPP" == "msvc") ]]
 
   for vs_release in "${VS_RELEASES[@]}"; do
     for program_files in "${PROGRAM_FILES[@]}"; do
-      for ((version = 2000; version <= 2100; version++)); do
+      for version in 18 {2000..2100}; do
         vs_dir="$program_files\\Microsoft Visual Studio\\$version\\$vs_release\\VC\\Auxiliary\\Build"
         if [[ -d $vs_dir ]]; then
           for bits in 32 64; do
